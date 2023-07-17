40e1ba134c5d74dd0be5fc294931e0d0ed7653a9
Carpet <c4rpetguy@gmail.com>
2023-07-17 10:04:53 +0300

    edit test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,6 @@
 from selenium.webdriver.common.by import By
 
-def test_user_can_see_purchase_button(browser):
+def test_basket_button_exists(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     basket_btn = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
